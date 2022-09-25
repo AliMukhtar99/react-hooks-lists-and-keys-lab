085671a27aba330438daf83d6773fd76ea4ddaed
Ali Mukhtar <ali.mukhtar@student.moringaschool.com>
2022-09-25 13:46:05 +0300

    completed lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,16 @@
-import React from "react";
+const links = ["home", "about", "projects"];
 
-function NavBar() {
-  const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
-}
+const NavBar = () => (
+  <nav>
+    {
+      /* display an <a> tag for each link here */
+      links.map((link) => (
+        <a href={`#${link}`} key={link}>
+          {link}
+        </a>
+      ))
+    }
+  </nav>
+);
 
 export default NavBar;
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,14 +1,25 @@
-import React from "react";
 import ProjectItem from "./ProjectItem";
 
-function ProjectList({ projects }) {
+const ProjectList = ({ projects }) => {
   console.log(projects);
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {
+          /* render ProjectItem components here */
+          projects.map((project) => (
+            <ProjectItem
+              name={project.name}
+              about={project.about}
+              technologies={project.technologies}
+              key={project.name}
+            />
+          ))
+        }
+      </div>
     </div>
   );
-}
+};
 
 export default ProjectList;
